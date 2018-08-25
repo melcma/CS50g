45bf0926f3e2b2ccf4c83d6c731531b298e17a5a
Adrian Piwowarczyk <adrpiw@gmail.com>
2018-08-25 21:54:58 +0100

    Displaying medals in score state

--- assignment1/states/ScoreState.lua
+++ assignment1/states/ScoreState.lua
@@ -14,8 +14,24 @@ ScoreState = Class{__includes = BaseState}
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
 ]]
+function ScoreState:init()
+    self.bronze = love.graphics.newImage('bronze-medal.png')
+    self.silver = love.graphics.newImage('silver-medal.png')
+    self.golden = love.graphics.newImage('golden-medal.png')
+end
+
 function ScoreState:enter(params)
     self.score = params.score
+
+    if self.score > 14 then
+        self.medal = self.golden
+    elseif self.score > 8 then
+        self.medal = self.silver
+    elseif self.score > 3 then
+        self.medal = self.bronze
+    else
+        self.medal = nil
+    end
 end
 
 function ScoreState:update(dt)
@@ -34,4 +50,8 @@ function ScoreState:render()
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
 
     love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+
+    if self.medal ~= nil then
+        love.graphics.draw(self.medal, VIRTUAL_WIDTH - 10 - self.medal:getWidth() / 2, 10, 0, 0.5, 0.5)
+    end
 end
