e42ededd61f05cc010fc6f2d80e6373d51dd41a5
Adrian Piwowarczyk <adrpiw@gmail.com>
2019-02-02 02:23:40 +0000

    Added Game Over scene

---
+++ assignment9/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+	public Component ambientSound;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (transform.position.y < -10)
+		{
+			if (ambientSound.gameObject != null)
+			{
+				Destroy(ambientSound.gameObject);
+			}
+			SceneManager.LoadScene("GameOver");
+		}
+    }
+}
