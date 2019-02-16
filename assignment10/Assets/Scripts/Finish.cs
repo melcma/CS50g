73baf59717c4ba9a6b27b97315a69ba1aa168382
Adrian Piwowarczyk <adrpiw@gmail.com>
2019-02-16 19:10:17 +0000

    Added GameOver text

--- assignment10/Assets/Scripts/Finish.cs
+++ assignment10/Assets/Scripts/Finish.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public Text text;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+	}
 
     // Update is called once per frame
     void Update()
@@ -18,6 +20,6 @@ public class Finish : MonoBehaviour
 
 	private void OnTriggerEnter(Collider other)
 	{
-		Debug.Log("entered");
+		text.text = "You Won!";
 	}
 }
